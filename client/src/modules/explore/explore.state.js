4e9c0178abaf0fc0e945564e9d4bfe7593713c0f
Brian Park <park-brian@users.noreply.github.com>
2021-06-14 12:58:08 -0400

    add state for explore module

---
+++ client/src/modules/explore/explore.state.js
@@ -0,0 +1,17 @@
+import { atom, selector } from "recoil";
+import { query } from "../../services/query";
+
+export const fieldsState = selector({
+  key: "explore.fieldsState",
+  get: ({ get }) => query("fields.json"),
+});
+
+export const defaultFormState = {
+  cancer: "",
+  gene: "",
+};
+
+export const formState = atom({
+  key: "explore.formState",
+  default: defaultFormState,
+});
