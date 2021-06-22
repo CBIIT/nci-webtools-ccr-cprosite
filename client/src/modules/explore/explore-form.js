cc6d5ebb7dcb589684c897ce9e885e8114975626
Brian Park <park-brian@users.noreply.github.com>
2021-06-22 16:46:20 -0400

    fix warnings

--- client/src/modules/explore/explore-form.js
+++ client/src/modules/explore/explore-form.js
@@ -1,15 +1,15 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Select from 'react-select';
-import { useRecoilState, useRecoilValue } from "recoil";
+import Select from "react-select";
+import { useRecoilValue } from "recoil";
 import { fieldsState, defaultFormState } from "./explore.state";
 import { useState } from "react";
 
 export default function ExploreForm({ onSubmit, onReset }) {
   const fields = useRecoilValue(fieldsState);
   const [form, setForm] = useState(defaultFormState);
-  const [correlation, setCorrelation] = useState('')
-  const [cancerList, setCancerList] = useState([])
+  const [correlation, setCorrelation] = useState("");
+  const [cancerList, setCancerList] = useState([]);
   const mergeForm = (obj) => setForm({ ...form, ...obj });
 
   function handleBlur(event) {
@@ -20,7 +20,6 @@ export default function ExploreForm({ onSubmit, onReset }) {
 
   function handleSubmit(event) {
     event.preventDefault();
-
     if (onSubmit) onSubmit(form);
   }
 
@@ -29,16 +28,14 @@ export default function ExploreForm({ onSubmit, onReset }) {
     if (onReset) onReset(defaultFormState);
   }
 
-  async function handleMultiChange(option) {
-
+  function handleMultiChange(option) {
     // Add all cancer types if "All Tumor Types" is selected
     if (option.length >= 1 && option[option.length - 1].value === 1) {
-      setCancerList(fields.cancer.slice(1))
-      await mergeForm({ ['cancer']: fields.cancer.slice(1) })
-    }
-    else {
-      setCancerList(option)
-      await mergeForm({ ['cancer']: option })
+      setCancerList(fields.cancer.slice(1));
+      mergeForm({ cancer: fields.cancer.slice(1) });
+    } else {
+      setCancerList(option);
+      mergeForm({ cancer: option });
     }
   }
 
@@ -46,8 +43,14 @@ export default function ExploreForm({ onSubmit, onReset }) {
     <Form onSubmit={handleSubmit} onReset={handleReset}>
       <Form.Group className="mb-3" controlId="cancer">
         <Form.Label className="required">Tumor Types</Form.Label>
-        <Select placeholder="No cancer selected" name="cancer" isMulti='true' value={cancerList} onChange={handleMultiChange} options={fields.cancer} />
-
+        <Select
+          placeholder="No cancer selected"
+          name="cancer"
+          isMulti="true"
+          value={cancerList}
+          onChange={handleMultiChange}
+          options={fields.cancer}
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="dataset">
@@ -64,11 +67,18 @@ export default function ExploreForm({ onSubmit, onReset }) {
         </Form.Select>
       </Form.Group>
 
-
-
       <Form.Group className="mb-3" controlId="analysis">
         <Form.Label className="required">Analysis</Form.Label>
-        <Form.Select name='analysis' onChange={(e) => { handleBlur(e); if(e.target.value === 'correlation') { setCorrelation('tumorVsControl') } else setCorrelation('')}} required>
+        <Form.Select
+          name="analysis"
+          onChange={(e) => {
+            handleBlur(e);
+            if (e.target.value === "correlation") {
+              setCorrelation("tumorVsControl");
+            } else setCorrelation("");
+          }}
+          required
+        >
           <option value="" hidden>
             No analysis selected
           </option>
@@ -99,41 +109,43 @@ export default function ExploreForm({ onSubmit, onReset }) {
         </datalist>
       </Form.Group>
 
-      <fieldset disabled={form.analysis !== 'correlation'} className="border px-3 mb-4">
+      <fieldset
+        disabled={form.analysis !== "correlation"}
+        className="border px-3 mb-4"
+      >
         <legend className="legend">Correlation</legend>
 
         <Form.Group className="row mb-3" controlId="correlation-type">
-          <Form.Check className="col-md-5" type='radio' inline>
+          <Form.Check className="col-md-5" type="radio" inline>
             <Form.Check.Input
-              type='radio'
-              name='correlation'
-              value='tumorVsControl'
-              checked={correlation === 'tumorVsControl'}
+              type="radio"
+              name="correlation"
+              value="tumorVsControl"
+              checked={correlation === "tumorVsControl"}
               onChange={(e) => {
                 setCorrelation(e.target.value);
                 handleBlur(e);
               }}
             />
-            <Form.Check.Label style={{ fontWeight: 'normal' }}>
+            <Form.Check.Label style={{ fontWeight: "normal" }}>
               Tumor vs Control
             </Form.Check.Label>
           </Form.Check>
-          <Form.Check className="col-md-5" type='radio' inline>
+          <Form.Check className="col-md-5" type="radio" inline>
             <Form.Check.Input
-              type='radio'
-              name='correlation'
-              value='geneVsGene'
-              checked={correlation === 'geneVsGene'}
+              type="radio"
+              name="correlation"
+              value="geneVsGene"
+              checked={correlation === "geneVsGene"}
               onChange={(e) => {
                 setCorrelation(e.target.value);
                 handleBlur(e);
               }}
             />
-            <Form.Check.Label style={{ fontWeight: 'normal' }}>
+            <Form.Check.Label style={{ fontWeight: "normal" }}>
               Gene vs Gene
             </Form.Check.Label>
           </Form.Check>
-
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="correlated-gene">
@@ -141,7 +153,7 @@ export default function ExploreForm({ onSubmit, onReset }) {
           <Form.Control
             name="correlated-gene"
             onBlur={handleBlur}
-            disabled={form.correlation !== 'geneVsGene'}
+            disabled={form.correlation !== "geneVsGene"}
             type="text"
             placeholder="No gene selected"
             list="genes"
