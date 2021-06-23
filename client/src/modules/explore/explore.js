f84da29e36fda05d39175e0f6d062039ff553124
Brian Park <park-brian@users.noreply.github.com>
2021-06-23 19:19:37 -0400

    show sample cases

--- client/src/modules/explore/explore.js
+++ client/src/modules/explore/explore.js
@@ -1,19 +1,24 @@
 import { Suspense } from "react";
+import { useRecoilState } from "recoil";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import ExploreForm from "./explore-form";
 import ErrorBoundary from "../components/error-boundary";
-
+import { formState } from "./explore.state";
 import Results from "./results";
 
 export default function Explore() {
+  const [form, setForm] = useRecoilState(formState);
+
   function handleSubmit(event) {
+    setForm(event);
     console.log("submit", event);
   }
 
   function handleReset(event) {
+    setForm(event);
     console.log("reset", event);
   }
 
@@ -34,8 +39,18 @@ export default function Explore() {
 
         <Col md={8}>
           <Card className="shadow">
-            <Card.Body>
-              <Results />
+            <Card.Body className="p-0">
+              <ErrorBoundary fallback="An unexpected error occured">
+                <Suspense fallback="Loading...">
+                  {![null, undefined, ""].includes(form.gene) ? (
+                    <Results />
+                  ) : (
+                    <h2 className="p-5 h5 d-flex align-items-center justify-content-center">
+                      Please Provide Search Parameters
+                    </h2>
+                  )}
+                </Suspense>
+              </ErrorBoundary>
             </Card.Body>
           </Card>
         </Col>
--- client/src/modules/explore/results.js
+++ client/src/modules/explore/results.js
@@ -1,7 +1,130 @@
+import { useRecoilValue } from "recoil";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import Table, { RangeFilter, TextFilter } from "../components/table";
+import Plot from "react-plotly.js";
+import { casesState } from "./explore.state";
+
 export default function Results() {
+  const cases = useRecoilValue(casesState);
+  const proteinAbundanceColumns = [
+    {
+      accessor: "name",
+      Header: "Patient ID",
+      Filter: TextFilter,
+    },
+    {
+      accessor: "proteinLogRatioControl",
+      Header: "Control Value",
+      Filter: RangeFilter,
+    },
+    {
+      accessor: "proteinLogRatioCase",
+      Header: "Case Value",
+      Filter: RangeFilter,
+    },
+    {
+      accessor: "proteinLogRatioChange",
+      Header: "Fold Change Value",
+      Filter: RangeFilter,
+    },
+  ];
+
+  const boxPlotData = [
+    {
+      y: cases.map((c) => c.proteinLogRatioControl),
+      type: "box",
+      boxpoints: "all",
+      name: "Case",
+    },
+    {
+      y: cases.map((c) => c.proteinLogRatioCase),
+      type: "box",
+      boxpoints: "all",
+      name: "Control",
+    },
+  ];
+
+  const average = (values) =>
+    values.filter((v) => v !== null).reduce((a, b) => a + b) / values.length;
+
+  const barPlotData = [
+    {
+      x: ["Control", "Case"],
+      y: [
+        average(cases.map((c) => c.proteinLogRatioControl)),
+        average(cases.map((c) => c.proteinLogRatioCase)),
+      ],
+      type: "bar",
+    },
+  ];
+
+  const defaultLayout = {
+    xaxis: {
+      zeroline: false,
+    },
+    yaxis: {
+      title: "Log Protein Abundance",
+      zeroline: false,
+    },
+    legend: {
+      itemsizing: "constant",
+      itemwidth: 40,
+    },
+    hovermode: "closest",
+  };
+
+  const defaultConfig = {
+    displayModeBar: true,
+    toImageButtonOptions: {
+      format: "svg",
+      filename: "plot_export",
+      height: 1000,
+      width: 1000,
+      scale: 1,
+    },
+    displaylogo: false,
+    modeBarButtonsToRemove: [
+      "select2d",
+      "lasso2d",
+      "hoverCompareCartesian",
+      "hoverClosestCartesian",
+    ],
+  };
+
   return (
-    <>
-      <div>Results Placeholder</div>
-    </>
+    <Tabs defaultActiveKey="proteinAbundance" className="mb-3">
+      <Tab eventKey="proteinAbundance" title="Protein Abundance">
+        <Row>
+          <Col xl={6}>
+            <Plot
+              data={boxPlotData}
+              layout={defaultLayout}
+              config={defaultConfig}
+              useResizeHandler
+              className="w-100"
+              style={{ height: "800px" }}
+            />
+          </Col>
+          <Col xl={6}>
+            <Plot
+              data={barPlotData}
+              layout={defaultLayout}
+              config={defaultConfig}
+              useResizeHandler
+              className="w-100"
+              style={{ height: "800px" }}
+            />
+          </Col>
+        </Row>
+
+        <Table
+          columns={proteinAbundanceColumns}
+          data={cases.filter((c) => c.proteinLogRatioControl !== null)}
+        />
+      </Tab>
+    </Tabs>
   );
 }
