eaf3d2c0fe3bf14e67519479f2f849b2d2ac4f32
Benjamin Chen <benchen4998@gmail.com>
2021-06-30 16:22:28 -0400

    add link to case tab in summary table labels

--- client/src/modules/explore/results.js
+++ client/src/modules/explore/results.js
@@ -3,15 +3,21 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button"
 import Table, { RangeFilter, TextFilter } from "../components/table";
 import Plot from "react-plotly.js";
 import { casesState, formState } from "./explore.state";
-
+import { useState } from "react";
 
 export default function Results() {
   const cases = useRecoilValue(casesState);
   const form = useRecoilValue(formState)
   const tumors = form.cancer.map((c) => c.value)
+  const [view, setView] = useState(tumors[0])
+  const [tab, setTab] = useState('summary')
+
+
 
   const proteinAbundanceColumns = [
     {
@@ -38,7 +44,7 @@ export default function Results() {
 
   const summaryColumns = [
     {
-      accessor: 'name',
+      accessor: 'link',
       Header: "Tumor Type",
       Filter: TextFilter,
     },
@@ -51,6 +57,16 @@ export default function Results() {
       accessor: 'caseAverage',
       Header: 'Average Case',
       Filter: RangeFilter,
+    },
+    {
+      accessor: 'controlNum',
+      Header: 'Control Count',
+      Filter: RangeFilter,
+    },
+    {
+      accessor: 'caseNum',
+      Header: 'Case Count',
+      Filter: RangeFilter,
     }
   ]
 
@@ -58,13 +74,13 @@ export default function Results() {
 
   const boxPlotData = [
     {
-      y: cases.filter((c) => tumors.includes(c.cancerId)).map((c) => c.proteinLogRatioControl),
+      y: cases.filter((c) => c.cancerId === view).map((c) => c.proteinLogRatioControl),
       type: "box",
       boxpoints: "all",
       name: "Control",
     },
     {
-      y: cases.filter((c) => tumors.includes(c.cancerId)).map((c) => c.proteinLogRatioCase),
+      y: cases.filter((c) => c.cancerId === view).map((c) => c.proteinLogRatioCase),
       type: "box",
       boxpoints: "all",
       name: "Case",
@@ -77,7 +93,7 @@ export default function Results() {
   function barPlotData(tumor) {
     return ([
       {
-        x: ["Control<br>n=" + tumor.controlNum, "Case<br>n=" + tumor.caseNum],
+        x: ["Control", "Case"],
         y: [
           tumor.controlAverage,
           tumor.caseAverage,
@@ -86,6 +102,7 @@ export default function Results() {
       },
     ])
   }
+
 
   const averages = form.cancer.map((c) => {
 
@@ -96,6 +113,7 @@ export default function Results() {
       {
         'id': c.value,
         'name': c.label,
+        'link': <a onClick={() => {setView(c.value); setTab('caseView')}} href='javascript:void(0)'>{c.label}</a>,
         'controlAverage': !isNaN(controlFilter[0]) ? average(controlFilter).toFixed(4) : 'NA',
         'caseAverage': !isNaN(caseFilter[0]) ? average(caseFilter).toFixed(4) : 'NA',
         'controlNum': !isNaN(controlFilter[0]) ? controlFilter.length : 0,
@@ -104,6 +122,23 @@ export default function Results() {
     )
 
   })
+
+  function multiBarPlotData() {
+    return ([
+      {
+        x: averages.map((c) => c.name),
+        y: averages.map((c) => c.controlAverage),
+        type: 'bar',
+        name: 'Control'
+      },
+      {
+        x: averages.map((c) => c.name),
+        y: averages.map((c) => c.caseAverage),
+        type: 'bar',
+        name: 'Case'
+      }
+    ])
+  }
 
   const defaultLayout = {
     xaxis: {
@@ -139,65 +174,73 @@ export default function Results() {
   };
 
   return (
-    <Tabs defaultActiveKey="proteinAbundance" className="mb-3">
+    <Tabs activeKey={tab} onSelect={(e) => setTab(e)} className="mb-3">
 
-      <Tab eventKey="proteinAbundance" title="Protein Abundance">
-        {averages.length > 1 ? <Row>
-          {
-            averages.map((e) => {
-              return (
-                <Col key={e.name} xl={3} md={6} >
-                  <Plot
-                    key={`${e.name}-plot`}
-                    data={barPlotData(e)}
-                    layout={{ ...defaultLayout, title: e.name }}
-                    config={defaultConfig}
-                    useResizeHandler
-                    className="flex-fill w-100"
-                    style={{ height: "500px" }}
-                  />
-                </Col>
-              )
+      <Tab eventKey="summary" title="Summary">
+        <Row>
+          <Col xl={12}>
+            <Plot
+              data={multiBarPlotData()}
+              layout={{ ...defaultLayout, barmode:'group' }}
+              config={defaultConfig}
+              useResizeHandler
+              className="flex-fill w-100"
+              style={{ height: "500px" }}
+            />
+          </Col>
+
+        </Row>
+
+        <Table
+          columns={summaryColumns}
+          data={averages}
+        />
+
+      </Tab>
+
+      <Tab eventKey="caseView" title="Case View">
+        <Form.Group className="m-3 col-xl-3" controlId="tumorView">
+          <Form.Label>Tumor Type</Form.Label>
+          <Form.Select name="tumorView" onChange={(e) => setView(parseInt(e.target.value))} value={view} required>
+            {form.cancer.map((o) => (
+              <option value={o.value} key={`dataset-${o.value}`}>
+                {o.label}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+        <Row>
+          <Col xl={6}>
+            <Plot
+              data={boxPlotData}
+              layout={defaultLayout}
+              config={defaultConfig}
+              useResizeHandler
+              className="w-100"
+              style={{ height: "800px" }}
+            />
+          </Col>
+          <Col xl={6}>
+            <Plot
+              data={barPlotData(averages.find(x => x.id === view))}
+              layout={defaultLayout}
+              config={defaultConfig}
+              useResizeHandler
+              className="w-100"
+              style={{ height: "800px" }}
+            />
+          </Col>
+        </Row>
+        <Table
+          columns={proteinAbundanceColumns}
+          data={cases.filter((c) => view === c.cancerId).map((c) => {
+            return ({
+              ...c,
+              proteinLogRatioCase: c.proteinLogRatioCase ? c.proteinLogRatioCase.toFixed(4) : 'NA',
+              proteinLogRatioControl: c.proteinLogRatioControl  ? c.proteinLogRatioControl.toFixed(4) : 'NA'
             })
-          }
-        </Row> :
-          <Row>
-            <Col xl={6}>
-              <Plot
-                data={boxPlotData}
-                layout={defaultLayout}
-                config={defaultConfig}
-                useResizeHandler
-                className="w-100"
-                style={{ height: "800px" }}
-              />
-            </Col>
-            <Col xl={6}>
-              <Plot
-                data={barPlotData(averages[0])}
-                layout={defaultLayout}
-                config={defaultConfig}
-                useResizeHandler
-                className="w-100"
-                style={{ height: "800px" }}
-              />
-            </Col>
-          </Row>}
-
-        {averages.length > 1 ?
-          <Table
-            columns={summaryColumns}
-            data={averages}
-          />
-          :
-          <Table
-            columns={proteinAbundanceColumns}
-            data={cases.filter((c) => tumors.includes(c.cancerId)).map((c) => { 
-              return({...c, 
-                proteinLogRatioCase: c.proteinLogRatioCase !== null ? c.proteinLogRatioCase.toFixed(4) : 'NA', 
-                proteinLogRatioControl: c.proteinLogRatioControl !== null ? c.proteinLogRatioControl.toFixed(4) : 'NA'})
-            })}
-          />}
+          })}
+        />
       </Tab>
     </Tabs>
   );
